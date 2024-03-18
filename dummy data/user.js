018e9f729a178de4df20b64cb8360c1f4b0cf2cb
unknown <akanshjain517@gmail.com>
2024-03-18 22:39:25 +0530

    BD

---
+++ dummy data/user.js
@@ -0,0 +1,40 @@
+[
+    {
+        "name": "Adarsh Kumar",
+        "age": 30,
+        "mobile": "9876543210",
+        "email": "ravi@example.com",
+        "address": "123, ABC Street, XYZ City",
+        "aadharCardNumber": 123456789012,
+        "password": "password123"
+    },
+    {
+        "name": "Ravina Negi",
+        "age": 28,
+        "mobile": "8765432109",
+        "email": "sita@example.com",
+        "address": "456, PQR Street, LMN City",
+        "aadharCardNumber": 234567890123,
+        "password": "password456"
+    },
+    {
+        "name": "Aarti Devi",
+        "age": 35,
+        "mobile": "7654321098",
+        "email": "amit@example.com",
+        "address": "789, DEF Street, UVW City",
+        "aadharCardNumber": 345678901234,
+        "password": "password789",
+        "role": "admin"
+    },
+    {
+        "name": "Priya",
+        "age": 25,
+        "mobile": "6543210987",
+        "email": "priya@example.com",
+        "address": "321, GHI Street, OPQ City",
+        "aadharCardNumber": 456789012345,
+        "password": "password123"
+    },
+    
+]
